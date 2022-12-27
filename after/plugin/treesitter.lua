f898345131f59cabe724516a6e8e4d9e93850634
root <root@CONOR-HOME>
2022-12-27 18:44:21 -0500

    removed treesitter install

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,8 +1,3 @@
-require'nvim-treesitter.install'.install {
-  compilers = { "gcc" }
-
-}
-
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
