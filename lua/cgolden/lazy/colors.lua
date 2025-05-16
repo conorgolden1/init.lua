2027f323f3b059cf429237b141235c6b3ef41e52
conorgolden1 <conorgolden1@hotmail.com>
2025-05-16 14:49:48 -0400

    changed colorscheme

--- lua/cgolden/lazy/colors.lua
+++ lua/cgolden/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -85,6 +85,10 @@ return {
 
             ColorMyPencils();
         end
+    },
+
+    {
+        "rebelot/kanagawa.nvim",
     },
 
 
